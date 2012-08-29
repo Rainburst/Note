73b964ef1db2fae835d0223cc71ad4c17c072470
Nikita Ilyasov <rainburst@gmail.com>
2012-08-29 22:12:19 +0400

    Initial Commit

---
+++ note/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  note
+//
+//  Created by Nikita Ilyasov on 29.08.12.
+//  Copyright (c) 2012 Hex Violet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
